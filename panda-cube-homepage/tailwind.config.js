fdda69ef305fc073946dcf0abf1e359053307e28
Alishba Javed <alishbajaved262@gmail.com>
2025-01-17 15:51:29 +0500

    leavessssssssssss

--- panda-cube-homepage/tailwind.config.js
+++ panda-cube-homepage/tailwind.config.js
@@ -8,29 +8,35 @@ export default {
   theme: {
     extend: {
       colors: {
-        'red-500': '#f87171',  // Tailwind red-500 color
+        gray: {
+          400: '#9CA3AF', // Extend gray-400
+          500: '#6B7280', // Extend gray-500
+        },
+        red: {
+          500: '#EF4444', // Extend red-500
+        },
+        primary: '#E84133',
+        secondary: '#1a5631',
+        darkBrown: '#a12217',
+      },
+      fontFamily: {
+        merriweather: ['Merriweather', 'serif'],
+        playfair: ['Playfair Display', 'serif'],
+        garamond: ['EB Garamond', 'serif'],
+        crimson: ['Crimson Text', 'serif'],
       },
     },
-    fontFamily: {
-      merriweather: ['Merriweather', 'serif'],
-      playfair: ['Playfair Display', 'serif'],
-      garamond: ['EB Garamond', 'serif'],
-      crimson: ['Crimson Text', 'serif'],
-    },
     colors: {
-      primary:'#E84133',
-      secondary:'#1a5631',
-      white:'#ffffff',
-      black:'#000000', 
+      white: '#ffffff',
+      black: '#000000',
       headerBg: 'var(--header-bg-color)',
-      headerText:'var(--header-text-color)',
-      heroText:'var(--hero-text-color)',
+      headerText: 'var(--header-text-color)',
+      heroText: 'var(--hero-text-color)',
       text: 'var(--grey-dark)',
-      heroSectionItems:'var(--hero-section-items)',
+      heroSectionItems: 'var(--hero-section-items)',
       footerBackgroundColor: 'var(--bg-footer-background)',
-      heroSectionButtonsBackground:'var(--heroSectionButtonsBackground)',
-      darkBrown:'#a12217',
+      heroSectionButtonsBackground: 'var(--heroSectionButtonsBackground)',
     },
   },
   plugins: [],
-}
+};
